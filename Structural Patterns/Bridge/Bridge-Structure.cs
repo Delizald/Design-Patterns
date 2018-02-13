b0679596c72084431e44ff9fb72d8e38999669ce
Delizald <david.elizalde.r.a@gmail.com>
2018-02-13 14:30:38 -0600

    Bridge structure

---
+++ Structural Patterns/Bridge/Bridge-Structure.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace BridgeStructure
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            Abstraction ab = new RefinedAbstraction();
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+
+            Console.ReadKey();
+        }
+
+        class Abstraction
+        {
+            protected Implementor implementor;
+
+            public Implementor Implementor
+            {
+                set {implementor = value;}
+            }
+
+            public virtual void Operation()
+            {
+                implementor.Operation();
+            }  
+        }
+
+        abstract class Implementor
+        {
+            public abstract void Operation();
+        } 
+
+        class RefinedAbstraction : Abstraction
+        {
+            public override void Operation()
+            {
+                implementor.Operation();
+            }
+        }
+
+        class ConcreteImplementorA : Implementor
+        {
+            public override void Operation()
+            {
+                Console.WriteLine("ConcreteImplementorA Operation");
+            }
+        }
+        
+        class ConcreteImplementorB : Implementor
+        {
+            public override void Operation()
+            {
+                Console.WriteLine("ConcreteImplementorB Operation");
+            }
+        }       
+    }
+}
